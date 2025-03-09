4c59867849092df6b0785d1e2770178169c81987
trancuong <brucelee1529@gmail.com>
2025-03-09 10:30:38 +0700

    Config express handlerbars

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+// import { engine } from 'express-handlebars';
+const handlebars = require('express-handlebars');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+app.engine('.hbs', handlebars.engine({ extname: '.hbs' }));
+app.set('view engine', '.hbs');
+// app.set('views', './src/resouces/views');
+app.set('views', path.join(__dirname, 'resources', 'views'));
+
+app.get('/home', (req, res) => {
+  res.render('home');
+});
+
+app.get('/news', (req, res) => {
+  res.render('news');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
