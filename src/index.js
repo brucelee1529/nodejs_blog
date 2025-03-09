2ba69a7d153da34ae8081d04722a42ab5e7c8fc3
trancuong <brucelee1529@gmail.com>
2025-03-09 14:25:39 +0700

    Config sass and bootstrap

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 const express = require('express');
-// import { engine } from 'express-handlebars';
 const handlebars = require('express-handlebars');
 const path = require('path');
 
 const app = express();
 const port = 3000;
 
+// config static files
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.engine('.hbs', handlebars.engine({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
 // app.set('views', './src/resouces/views');
 app.set('views', path.join(__dirname, 'resources', 'views'));
 
-app.get('/home', (req, res) => {
+app.get('/', (req, res) => {
   res.render('home');
 });
 
